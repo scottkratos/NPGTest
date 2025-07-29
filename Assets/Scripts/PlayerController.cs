9128c7342c6ebeb191d31d7f926cc4c18df44578
Gabriel Caldeira <48250822+scottkratos@users.noreply.github.com>
2025-07-28 22:11:01 -0300

    Player movement base

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +12,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private Vector2 movement;
     private Rigidbody rb;
+    private Quaternion desiredRotation;
+    private float lerpTimer = .2f;
+    private Coroutine lerpCoroutine;
 
     private void Awake()
     {
@@ -31,8 +36,30 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector3(movement.x * speed, rb.linearVelocity.y, movement.y * speed);
-        print(rb.linearVelocity);
-        //transform.localRotation = Quaternion.Euler(new Vector3(1, 1, 1));
+        if (movement.magnitude > 0.1f)
+        {
+            if (desiredRotation != Quaternion.Euler(0, Mathf.Atan2(-movement.y, movement.x) * Mathf.Rad2Deg - 90, 0))
+            {
+                desiredRotation = Quaternion.Euler(0, Mathf.Atan2(-movement.y, movement.x) * Mathf.Rad2Deg - 90, 0);
+                if (lerpCoroutine != null) StopCoroutine(lerpCoroutine);
+                StartCoroutine(LerpRotation());
+            }
+        }
+        else if (lerpCoroutine != null) StopCoroutine(lerpCoroutine);
+    }
+
+    private IEnumerator LerpRotation()
+    {
+        float localTimer = 0;
+        Quaternion initialRot = transform.localRotation;
+
+        while (localTimer < lerpTimer)
+        {
+            transform.localRotation = Quaternion.Lerp(initialRot, desiredRotation, localTimer / lerpTimer);
+            localTimer += Time.deltaTime;
+            yield return null;
+        }
+        transform.localRotation = desiredRotation;
     }
 
     private void OnEnable()
