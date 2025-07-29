5429f6e5d1d6e0a87b98ae071e7525df902d4abb
Gabriel Caldeira <48250822+scottkratos@users.noreply.github.com>
2025-07-29 18:32:15 -0300

    Disable save for tests

--- Assets/Scripts/Interactables/SaveStation.cs
+++ Assets/Scripts/Interactables/SaveStation.cs
@@ -5,6 +5,6 @@ public class SaveStation : InteractiveBase
     public override void Use(PlayerController player)
     {
         base.Use(player);
-        SaveManager.instance.SaveCurrentProgress();
+        //SaveManager.instance.SaveCurrentProgress();
     }
 }
